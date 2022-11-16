3adc006f9e0497a0a5d30222f24eb20dfb2f6497
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-11-16 22:58:50 +0000

    Create variables to store item data and render them

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+
+    const expenseDate = new Date(2022, 11, 16);
+    const expenseTitle = "House Insurance";
+    const expenseAmount = 185.48;
+
     return (
         <div className='expense-item'>
-            <div className='expense-item__description'>16/11/2022</div>
+            <div className='expense-item__description'>{expenseDate.toISOString()}</div>
             <div className='expense-item__price'>
-                <h2>House Insurance</h2>
-                <div>$200</div>
+                <h2>{expenseTitle}</h2>
+                <div>{expenseAmount}</div>
             </div>
         </div>
     )
