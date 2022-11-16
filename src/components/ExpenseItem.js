1779e86a8043fbea64f99503583d218a2444d52a
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-11-16 22:47:17 +0000

    Add basic styling to ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+    return (
+        <div className='expense-item'>
+            <div className='expense-item__description'>16/11/2022</div>
+            <div className='expense-item__price'>
+                <h2>House Insurance</h2>
+                <div>$200</div>
+            </div>
+        </div>
+    )
 }
 
 export default ExpenseItem;
