76579c137542b6cdcf15d899319da3617c4bc708
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-11-17 20:13:02 +0000

    Add clickHandler event listener

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,13 @@
 import './ExpenseItem.css'
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
+import { click } from '@testing-library/user-event/dist/click';
 
 const ExpenseItem = (props) => {
+
+    const clickHandler = () => {
+        alert('Clicked!')
+    }
 
     return (
         <Card className='expense-item'>
@@ -11,6 +16,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change title</button>
         </Card>
     )
 }
