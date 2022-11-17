b688c1ca6faca483d655e6125acbf2b7fb1099d1
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-11-17 15:11:41 +0000

    Create ExpenseDate component, add styles

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     {
       title: 'House Insurance',
       amount: 176.56,
-      date: new Date(2022, 11, 17)
+      date: new Date(2022, 10, 10)
     },
     {
       title: 'Life Insurance',
@@ -16,15 +16,15 @@ function App() {
     {
       title: 'Car Insurance',
       amount: 100.86,
-      date: new Date(2022, 11, 17)
+      date: new Date(2022, 5, 14)
     }
   ]
 
   return (
     <div className="App">
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
     </div>
   )
 }
