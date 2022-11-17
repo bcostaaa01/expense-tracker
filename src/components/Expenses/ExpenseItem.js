c668472c61f1494bf9c27f326fddc32d486bcbec
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-11-17 20:24:19 +0000

    Create state variable for title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,21 @@
+import React, {useState} from 'react'
 import './ExpenseItem.css'
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
-import { click } from '@testing-library/user-event/dist/click';
 
 const ExpenseItem = (props) => {
 
+    const [title, setTitle] = useState(props.title)
+
     const clickHandler = () => {
-        alert('Clicked!')
+        setTitle('Testing state')
     }
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change title</button>
