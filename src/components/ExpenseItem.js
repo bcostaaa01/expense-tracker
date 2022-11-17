66e23d291b03068864f73b450fa76bc1846a39f7
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-11-17 15:01:13 +0000

    create variables to store date data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,14 +2,26 @@ import './ExpenseItem.css'
 
 function ExpenseItem(props) {
 
-
+    const month = props.date.toLocaleString('en-US', {
+        month: 'long'
+    })
+    const day = props.date.toLocaleString('en-US', {
+        day: '2-digit'
+    })
+    const year = props.date.getFullYear()
 
     return (
         <div className='expense-item'>
-            <div className='expense-item__description'>{props.date.toISOString()}</div>
-            <div className='expense-item__price'>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
+            <div className='expense-item__description'>
                 <h2>{props.title}</h2>
-                <div>{props.amount}</div>
+                <div className='expense-item__price'>
+                    <div>{props.amount}</div>
+                </div>
             </div>
         </div>
     )
