deab2820771db01e3d15179a752c4b7593aec103
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-11-18 18:11:17 +0000

    Comment out alternative state management code

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
@@ -6,17 +6,42 @@ const ExpenseForm = () => {
     const [enteredTitle, setEnteredTitle] = useState('')
     const [enteredAmount, setEnteredAmount] = useState('')
     const [enteredDate, setEnteredDate] = useState('')
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+    // })
 
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value)
+    //     setUserInput(
+    //         // {
+    //         //     ...userInput,
+    //         //     enteredTitle: event.target.value
+    //         // }
+    //         (prevState) => {
+    //             return {
+    //                 ...prevState, enteredTitle: event.target.value
+    //             }
+    //         })
     }
 
     const amountChangeHandler = (event) => {
         setEnteredAmount(event.target.value)
+    //     setUserInput(
+    //         {
+    //             ...userInput,
+    //             enteredAmount: event.target.value
+    //         })
     }
 
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value)
+    //     setUserInput(
+    //         {
+    //             ...userInput,
+    //             enteredDate: event.target.value
+    //         })
     }
 
     return (
