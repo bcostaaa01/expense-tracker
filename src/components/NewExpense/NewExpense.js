5e30b4ab6c8c7e48eeacc77cc33de50e8dab0032
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-11-18 18:17:26 +0000

    Format code

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,13 +3,11 @@ import './NewExpense.css'
 import ExpenseForm from './ExpenseForm'
 
 const NewExpense = () => {
-  return (
-    <div className='new-expense'>
-
+    return (
+        <div className='new-expense'>
             <ExpenseForm />
-
-    </div>
-  )
+        </div>
+    )
 }
 
 export default NewExpense
