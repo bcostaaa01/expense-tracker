a4715ce9ba34d92b04101be77278438e8575b558
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-11-19 15:08:32 +0000

    Add state to App.js to render new expense component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
+import { useState } from "react";
+
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    title: 'House Insurance',
+    amount: 176.56,
+    date: new Date(2022, 10, 10)
+  },
+  {
+    title: 'Life Insurance',
+    amount: 145.26,
+    date: new Date(2022, 11, 17)
+  },
+  {
+    title: 'Car Insurance',
+    amount: 100.86,
+    date: new Date(2022, 5, 14)
+  }
+]
+
 const App = () => {
 
-  const expenses = [
-    {
-      title: 'House Insurance',
-      amount: 176.56,
-      date: new Date(2022, 10, 10)
-    },
-    {
-      title: 'Life Insurance',
-      amount: 145.26,
-      date: new Date(2022, 11, 17)
-    },
-    {
-      title: 'Car Insurance',
-      amount: 100.86,
-      date: new Date(2022, 5, 14)
-    }
-  ]
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
 
   const addExpenseHandler = (expense) => {
-    console.log('In App.js')
-    console.log(expense)
+    setExpenses(prevExpenses =>
+      [expense, ...prevExpenses])
   }
 
   return (
     <div className="App">
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses}/>
+      <Expenses items={expenses} />
     </div>
   )
 }
