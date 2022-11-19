42b2cee70d4bb38df901f174c7eacd180663753e
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-11-19 15:21:16 +0000

    filter expenses to choose between year to render specific expense components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,11 +12,17 @@ const Expenses = (props) => {
         setFilteredYear(selectedYear)
     }
 
+    const filteredExpenses = 
+        props.items.filter(expense => {
+            return expense.date.getFullYear().toString() === filteredYear
+        })
+    
+
     return (
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {props.items.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />)}
+                {filteredExpenses.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />)}
             </Card>
         </div>
     )
