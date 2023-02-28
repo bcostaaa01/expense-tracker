978b2656a0b3700631ac4efc7b86e30c81dead02
Bruno Costa <bcostinha01@gmail.com>
2023-02-28 22:43:41 +0000

    Add condition to render if there are no expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,36 +1,32 @@
-import React, { useState } from 'react'
-import ExpenseItem from './ExpenseItem'
-import Card from '../UI/Card'
-import ExpensesFilter from './ExpensesFilter'
-import './Expenses.css'
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2019");
 
-    const [filteredYear, setFilteredYear] = useState('2019')
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
-    const filterChangeHandler = (selectedYear) => {
-        setFilteredYear(selectedYear)
-    }
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
-    const filteredExpenses =
-        props.items.filter(expense => {
-            return expense.date.getFullYear().toString() === filteredYear
-        })
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
 
-    const expensesContent = <p>No expenses found.</p>
-
-    if (filteredExpenses.length === 0) {
-        expensesContent = filteredExpenses.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />)
-    }
-
-    return (
-        <div>
-            <Card className='expenses'>
-                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {expensesContent}
-            </Card>
-        </div>
-    )
-}
-
-export default Expenses
+export default Expenses;
